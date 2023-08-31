1f4518c94b7ac42a241c1ffd01b2ce6c02711139
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-08-31 21:31:04 +0800

    Add fragments

---
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/SignInViewPageAdapter.java
@@ -0,0 +1,35 @@
+package com.example.cs426_final_project;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class SignInViewPageAdapter extends FragmentStateAdapter {
+
+    final String[] titles = {"Welcome", "EmailReceiver"};
+
+    public SignInViewPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0)
+            return new WelcomeFragment();
+        if (position == 1)
+            return new EmailReceiverFragment();
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.length;
+    }
+}
