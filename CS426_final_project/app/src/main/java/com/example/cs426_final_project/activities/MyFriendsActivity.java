0b41eb380d1a400d072b3f928124d621b3c8a781
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-01 22:02:50 +0800

    Enable button

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/MyFriendsActivity.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/MyFriendsActivity.java
@@ -3,6 +3,8 @@ package com.example.cs426_final_project.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.cs426_final_project.R;
 
@@ -11,6 +13,19 @@ public class MyFriendsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_friends);
+        this.setContentView(R.layout.activity_my_friends);
+        this.enableX();
     }
+
+    private void enableX() {
+        ImageView button = this.findViewById(R.id.x_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
 }
