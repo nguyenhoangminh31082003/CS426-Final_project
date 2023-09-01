9d8ca60c6e6ca5955dc65b57bc6db254c2ddf79a
htamlive <nghichmaychoi@gmail.com>
2023-09-01 09:06:23 +0800

    Modify .xml to have better display

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/MainActivity.kt
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,11 +21,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             CS426_final_projectTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+//                Column(
+//                    modifier = Modifier.fillMaxWidth()
+//                ) {
+//                    Text(
+//                        text = "Hello World!",
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+//                    Text(
+//                        text = "Nice to meet you!",
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+//                }
             }
         }
         val intent = Intent(this, SignInPage::class.java)
