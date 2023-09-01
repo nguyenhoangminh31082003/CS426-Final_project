1b014aa9e3a97f18d45c873a04dfc0e3e9674c45
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-01 21:26:57 +0800

    Add MyFriendsActivity

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/MainActivity.kt
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
             }
         }
         //val intent = Intent(this, SignInActivity::class.java)
-        val intent = Intent(this, FoodScanActivity::class.java)
+        val intent = Intent(this, SignInActivity::class.java)
         startActivity(intent)
     }
 }
