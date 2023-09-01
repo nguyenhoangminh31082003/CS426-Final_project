fe3e643fefe567196ec33cc4e87d29cfe65f236d
htamlive <nghichmaychoi@gmail.com>
2023-09-01 11:08:32 +0800

    Create animations for welcome screen

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/WelcomeFragment.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/WelcomeFragment.java
@@ -48,11 +48,6 @@ public class WelcomeFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
