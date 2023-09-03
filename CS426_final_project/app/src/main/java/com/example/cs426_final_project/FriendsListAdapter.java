d39b8f37eeb39424cf95205191b00ed15d26c6c6
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-03 15:02:51 +0800

    Add adapter for friend list

---
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/FriendsListAdapter.java
@@ -0,0 +1,27 @@
+package com.example.cs426_final_project;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class FriendsListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
