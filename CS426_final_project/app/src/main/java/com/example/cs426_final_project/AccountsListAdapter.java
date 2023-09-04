25ef44517a9bbad31b8c8851253b4d23ced06580
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-05 01:28:48 +0800

    Update adapter

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/AccountsListAdapter.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/AccountsListAdapter.java
@@ -8,42 +8,68 @@ import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class AccountsListAdapter extends BaseExpandableListAdapter {
 
+    final private static int LIMIT_NUMBER_OF_SUGGESTIONS = 5;
+    final private static int LIMIT_NUMBER_OF_FRIENDS = 30;
+
+    final private static String SUGGESTIONS_HEADER = "Suggestion";
+    final private static String FRIENDS_HEADER = "Friends";
+    final private static String[] headers = {SUGGESTIONS_HEADER, FRIENDS_HEADER};
     private Activity activity;
+    private HashMap<String, ListOfAccountRows> accounts;
+
+    private static ListOfAccountRows getListOfSuggestions() {
+        return new ListOfAccountRows();
+    }
+
+    private static ListOfAccountRows getListOfFriends() {
+        return new ListOfAccountRows();
+    }
+
+    private static HashMap<String, ListOfAccountRows> getListOfAccounts() {
+        HashMap<String, ListOfAccountRows> accounts = new HashMap<String, ListOfAccountRows>();
+        accounts.put(headers[0], getListOfSuggestions());
+        accounts.put(headers[1], getListOfFriends());
+        return accounts;
+    }
+
 
     public AccountsListAdapter(Activity activity) {
         this.activity = activity;
+        this.accounts = getListOfAccounts();
     }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return headers.length;
     }
 
     @Override
-    public int getChildrenCount(int i) {
-        return 0;
+    public int getChildrenCount(final int i) {
+        return this.accounts.get(headers[i]).getNumberOfRows();
     }
 
     @Override
-    public Object getGroup(int i) {
-        return null;
+    public Object getGroup(final int i) {
+        return headers[i];
     }
 
     @Override
-    public Object getChild(int i, int i1) {
-        return null;
+    public Object getChild(final int x, final int y) {
+        return this.accounts.get(headers[x]).getAccountRow(y);
     }
 
     @Override
-    public long getGroupId(int i) {
-        return 0;
+    public long getGroupId(final int i) {
+        return i;
     }
 
     @Override
-    public long getChildId(int i, int i1) {
-        return 0;
+    public long getChildId(final int x, final int y) {
+        return y;
     }
 
     @Override
--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/ListOfFriendRows.java
+++
@@ -1,9 +0,0 @@
-package com.example.cs426_final_project;
-
-public class ListOfFriendRows extends ListOfAccountRows {
-
-    public ListOfFriendRows() {
-        super();
-    }
-
-}
