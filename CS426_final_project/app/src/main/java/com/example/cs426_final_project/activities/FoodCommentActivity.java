4d12e7dba0aaba3fdb13e62c33bb37460277a94c
htamlive <nghichmaychoi@gmail.com>
2023-09-05 10:49:03 +0800

    fix image rendering complete

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/FoodCommentActivity.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/FoodCommentActivity.java
@@ -3,6 +3,7 @@ package com.example.cs426_final_project.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -27,10 +28,21 @@ public class FoodCommentActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         Uri previewImageUri = Uri.parse(intent.getStringExtra("imageUri"));
+//        Bitmap previewImageBitmap = BitmapFactory.decodeFile(previewImageUri.getPath());
+        // print width and height of bitmap
+//        System.out.println("width: " + previewImageBitmap.getWidth() + " height: " + previewImageBitmap.getHeight());
 
         ImageView ivPreviewImage = findViewById(R.id.ivPreviewImage);
         ivPreviewImage.setImageURI(previewImageUri);
 
+        // preserve ratio 1:1 of ivPreviewImage
+        ivPreviewImage.setAdjustViewBounds(true);
+        ivPreviewImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+
+
+//        Bitmap bitmap = ((BitmapDrawable)ivPreviewImage.getDrawable()).getBitmap();
+//        System.out.println("width: " + bitmap.getWidth() + " height: " + bitmap.getHeight());
 
 
     }
