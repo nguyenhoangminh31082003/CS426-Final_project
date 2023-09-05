791c19c913ebfa8ed9eb57db684798691050ea01
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-05 16:52:13 +0800

    Add button to flow layout

---
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/TypeOptionsStatus.java
@@ -0,0 +1,34 @@
+package com.example.cs426_final_project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeOptionsStatus {
+
+    private List<String> values;
+    private List<Boolean> chosen;
+
+    public int getSize() {
+        return this.values.size();
+    }
+
+    public TypeOptionsStatus(final List<String> values) {
+        this.values = values;
+        this.chosen = new ArrayList<Boolean>();
+        for (int i = values.size() - 1; i >= 0; --i)
+            this.chosen.add(false);
+    }
+
+    public boolean toggle(final int position) {
+        if ((position < 0) || (position >= this.values.size()))
+            return false;
+        this.chosen.set(position, !(this.chosen.get(position)));
+        return true;
+    }
+
+    public String getTypeOption(final int position) {
+        if ((position < 0) || (position >= this.values.size()))
+            return null;
+        return this.values.get(position);
+    }
+}
--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/SearchActivity.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/SearchActivity.java
@@ -3,16 +3,46 @@ package com.example.cs426_final_project.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SearchView;
 
+import com.example.cs426_final_project.MutableTypeOptionsData;
 import com.example.cs426_final_project.R;
+import com.example.cs426_final_project.TypeOptionsStatus;
+import com.google.android.material.internal.FlowLayout;
 
 public class SearchActivity extends AppCompatActivity {
 
+    TypeOptionsStatus typeOptionsStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_search);
+        this.setTypeOptionsStatus();
+    }
+
+    public void setTypeOptionsStatus() {
+        FlowLayout flowLayout = this.findViewById(R.id.flow_list_of_type_options);
+        this.typeOptionsStatus = new TypeOptionsStatus((new MutableTypeOptionsData()).toList());
+
+        final int numberOfOptions = this.typeOptionsStatus.getSize();
+
+        for (int i = 0; i < numberOfOptions; ++i) {
+            final int immutable_i = i;
+            Button button = new Button(this);
+
+            button.setText(this.typeOptionsStatus.getTypeOption(i));
+            //button.setTextColor();
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    typeOptionsStatus.toggle(immutable_i);
+                }
+            });
+
+            flowLayout.addView(button);
+        }
     }
 }
