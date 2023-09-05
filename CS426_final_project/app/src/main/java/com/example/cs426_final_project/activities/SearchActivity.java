8ea77dbfdd7ed646ab7e20d21ccd2395e3f6e2d8
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-05 21:25:20 +0800

    Cusomize search bar in search page

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/SearchActivity.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/SearchActivity.java
@@ -8,6 +8,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SearchView;
 
@@ -24,9 +25,25 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_search);
         this.setTypeOptionsStatus();
+        this.setSearchView();
     }
 
-    public void setTypeOptionsStatus() {
+    private void setSearchView() {
+        androidx.appcompat.widget.SearchView searchView = this.findViewById(R.id.search_view_in_search_page);
+        EditText editText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        ImageView closeButton = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+        ImageView searchMagIcon = searchView.findViewById(androidx.appcompat.R.id.search_mag_icon);
+        //ImageView openButton = searchView.findViewById(androidx.appcompat.R.id.search_mag_icon);
+        //ImageView openButton = searchView.findViewById(androidx.appcompat.R.id.search_go_btn);
+        ImageView openButton = searchView.findViewById(androidx.appcompat.R.id.search_button);
+        openButton.setImageResource(R.drawable.search_page_search_icon);
+        searchMagIcon.setImageResource(R.drawable.search_page_search_icon);
+        closeButton.setImageResource(R.drawable.search_page_close_icon);
+        editText.setHintTextColor(ContextCompat.getColor(this, R.color.white));
+        searchView.setQueryHint("Type here to search");
+    }
+
+    private void setTypeOptionsStatus() {
         com.nex3z.flowlayout.FlowLayout flowLayout = this.findViewById(R.id.flow_list_of_type_options);
         this.typeOptionsStatus = new TypeOptionsStatus((new MutableTypeOptionsData()).toList());
 
@@ -42,8 +59,9 @@ public class SearchActivity extends AppCompatActivity {
                         ViewGroup.LayoutParams.WRAP_CONTENT
                     )
             );
+            button.setPadding(4, 0, 4, 0);
             button.setText(this.typeOptionsStatus.getTypeOption(i));
-            button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+            button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
             button.setTextColor(ContextCompat.getColor(this, R.color.white));
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
