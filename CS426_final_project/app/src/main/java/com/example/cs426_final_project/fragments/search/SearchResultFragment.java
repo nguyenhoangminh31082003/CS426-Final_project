0afeab0816ae47e191af13d7a7bb7961d4a55bba
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-06 10:41:23 +0800

    Set Layout Manager for list of food in search result

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/fragments/search/SearchResultFragment.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/fragments/search/SearchResultFragment.java
@@ -3,6 +3,8 @@ package com.example.cs426_final_project.fragments.search;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -20,6 +22,14 @@ public class SearchResultFragment extends Fragment {
         this.searchQuery = searchQuery;
     }
 
+    private void setUpListOfFoods(@NonNull View view) {
+        RecyclerView viewOfListOfFoods = view.findViewById(R.id.list_of_foods_in_search_result);
+
+        //viewOfListOfFoods.setAdapter(adapter);
+
+        viewOfListOfFoods.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,8 +39,8 @@ public class SearchResultFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView tvNoResult = view.findViewById(R.id.tvNoResult);
-        tvNoResult.setText(String.format("No result for %s", searchQuery));
-
+        //TextView tvNoResult = view.findViewById(R.id.tvNoResult);
+        //tvNoResult.setText(String.format("No result for %s", searchQuery));
+        this.setUpListOfFoods(view);
     }
 }
