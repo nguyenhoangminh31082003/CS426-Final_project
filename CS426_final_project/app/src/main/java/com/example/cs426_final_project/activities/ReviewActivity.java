8851f1ede1e6e967931023401c8caac8e1b93910
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2023-09-06 22:55:52 +0800

    Create layout for review

--- CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/ReviewActivity.java
+++ CS426_final_project/app/src/main/java/com/example/cs426_final_project/activities/ReviewActivity.java
@@ -1,6 +1,8 @@
 package com.example.cs426_final_project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,9 +10,17 @@ import com.example.cs426_final_project.R;
 
 public class ReviewActivity extends AppCompatActivity {
 
+    private RecyclerView listOfReviewsView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        this.setContentView(R.layout.activity_review);
+        this.setUpListOfReviewsView();
+    }
+
+    private void setUpListOfReviewsView() {
+        this.listOfReviewsView = this.findViewById(R.id.review_food_reviews_list);
+
+        this.listOfReviewsView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
